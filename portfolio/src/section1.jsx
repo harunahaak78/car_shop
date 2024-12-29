ead30d6e08d5e024851d74bb43fd611a8550ca27
harunahaak78 <abdulhaakharuna@gmail.com>
2024-12-29 01:03:15 -0800

    creating navbar and intro section

---
+++ portfolio/src/section1.jsx
@@ -0,0 +1,11 @@
+import  Profiler  from "./profile-1.jpg";
+function Intro(){
+    return(
+        <div className="container-fluid intro ">
+            <img src={Profiler} class="rounded float-start" alt="PROFILER-PIC"></img>
+        
+        </div>
+    );
+
+}
+export default Intro;
