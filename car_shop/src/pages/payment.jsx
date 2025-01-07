d4fc478c916334c6853ffb62624b6f9f7be2c992
harunahaak78 <abdulhaakharuna@gmail.com>
2025-01-07 11:39:29 -0800

    Designing the payment page

--- car_shop/src/pages/payment.jsx
+++ car_shop/src/pages/payment.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom';
+import Navbar from '../component/navbar';
 
 const PaymentPage = () => {
   const { state: product } = useLocation();
@@ -8,9 +9,13 @@ const PaymentPage = () => {
   }
 
   return (
-    <div className="container mt-5">
-      <h2>Payment Page</h2>
-      <div className="card w-50 mx-auto mt-3">
+    <>
+    <Navbar/>
+     <div className="container mt-5">
+      <h2 className=" display-4 text-center">Payment Page</h2>
+      <div className="row mb-3">
+        <div className="col">
+        <div className="card w-50 mx-auto mt-3">
         <img
           src={require(`../img/${product.image}`)}
           className="card-img-top"
@@ -23,8 +28,19 @@ const PaymentPage = () => {
           <button className="btn btn-primary">Proceed to Payment</button>
         </div>
       </div>
+        </div>
+        <div className="col">
+          <h5>Fill up your Personal information</h5>
+          <input type='text' placeholder='First Name' className=' border-none rounde' /><input type='text' placeholder='Last Name' className='ms-2'/>
+        </div>
+      </div>
+ 
     </div>
+    </>
+  
   );
 };
+
+
 
 export default PaymentPage;
